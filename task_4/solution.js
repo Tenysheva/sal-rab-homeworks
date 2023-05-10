bd8312675025a9794b32f0b021d2d3bb4ec90b41
Dasha <tenyshevadasha@mail.ru>
2023-05-10 12:59:36 +0300

    change 3

--- task_4/solution.js
+++ task_4/solution.js
@@ -8,7 +8,6 @@ function renderProductsCards(json) {
     clearProducts();
     let products = parseProducts(json);
     let length = products.length;
-
 for (let i = 0; i < length; i += 1) {
     addProduct(products[i]);
 }
