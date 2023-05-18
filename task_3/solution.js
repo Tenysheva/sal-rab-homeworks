1b2c387fc97f48434b1030d04bc136e82f78686b
Dasha <tenyshevadasha@mail.ru>
2023-05-18 13:28:46 +0300

    change 3

--- task_3/solution.js
+++ task_3/solution.js
@@ -14,15 +14,14 @@ function renderCartItem(item) {
     // количество × цена ₽ = стоимость ₽
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
-let itemCountText = " ";
+let itemCountText = "";
 itemCountText = itemCountText + product.count;
-itemCountText = itemCountText + ` × `;
+itemCountText = itemCountText + " × ";
 itemCountText = itemCountText + product.price;
-itemCountText = itemCountText + ` ₽ = `;
+itemCountText = itemCountText + " ₽ = ";
 let sum = product.count * product.price;
 itemCountText = itemCountText + sum;
-itemCountText = itemCountText + ` ₽ `;
-
+itemCountText = itemCountText + " ₽";
     // Конец решения задания №3.1.
 
  return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
