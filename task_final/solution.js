a2e06b33a54c8b74686333dc765ca95eacb8aecc
Dasha <tenyshevadasha@mail.ru>
2023-05-19 12:06:40 +0300

    change 19/05

--- task_final/solution.js
+++ task_final/solution.js
@@ -37,7 +37,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
 let countOfGoods = goods.length;
 for (let i = 0; i < countOfGoods; i += 1) {
-    data.goods.push({title:goods[i].title, count:goods[i].count});
+data.goods.push({title:goods[i].title, count:goods[i].count});
 }
 data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
 data.order.sum = sum;
